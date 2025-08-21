ffa46f325e8407f1ef72afc60ebf0f48d22b6ddc
Josasd10 <joseschumann07@gmail.com>
2025-08-21 17:38:48 -0600

    Update Encargado.java

--- Encargado.java
+++ Encargado.java
@@ -2,4 +2,34 @@ package controlador;
 
 public class Encargado {
 
+ 
+    private String nombre;
+    private String puesto;
+    private int id;
+
+   
+    public Encargado() { }
+
+    public Encargado(String nombre, String puesto, int id) {
+        this.nombre = nombre;
+        this.puesto = puesto;
+        this.id = id;
+    }
+
+    
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public String getPuesto() { return puesto; }
+    public void setPuesto(String puesto) { this.puesto = puesto; }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    
+    @Override
+    public String toString() {
+        return "Encargado{id=" + id + ", nombre='" + nombre + "', puesto='" + puesto + "'}";
+    }
 }
+
